549016a68d0c1ed81c47ed7d9272a41713bb7f1f
MartinezFabian <FabianMartinezDev@gmail.com>
2022-05-07 11:57:38 -0300

    Se agrega el proyecto

---
+++ EjercicioFechas/GestionFechas.cs
@@ -0,0 +1,77 @@
+﻿namespace EjercicioFechas
+{
+    internal class GestionFechas
+    {
+        public int obtenerDiasCalendario(DateTime fechaUno, DateTime fechDos)
+        {
+            TimeSpan diferencia = TimeSpan.Zero;
+
+            int resultado = DateTime.Compare(fechaUno, fechDos);
+
+            if (resultado > 0)
+            {
+                diferencia = fechaUno - fechDos;
+            }
+            else if (resultado < 0)
+            {
+                diferencia = fechDos - fechaUno;
+            }
+            else if (resultado == 0)
+            {
+                diferencia = TimeSpan.Zero;
+            }
+
+            return diferencia.Days;
+        }
+
+        public int obtenerDiasLaborales(DateTime fechaUno, DateTime fechDos)
+        {
+            DateTime[] feriados =
+            {
+                new DateTime(2022, 01, 01),
+                new DateTime(2022, 02, 28),
+                new DateTime(2022, 03, 01),
+                new DateTime(2022, 03, 24),
+                new DateTime(2022, 04, 02),
+                new DateTime(2022, 04, 15),
+                new DateTime(2022, 05, 01),
+                new DateTime(2022, 05, 25),
+            };
+
+            int cont = 0;
+            bool esFeriado = false;
+
+            for (DateTime fecha1 = fechaUno; fecha1 <= fechDos; fecha1 = fecha1.AddDays(1))
+            {
+                if (fecha1.DayOfWeek != DayOfWeek.Saturday && fecha1.DayOfWeek != DayOfWeek.Sunday)
+                {
+                    for (int i = 0; i < feriados.Length; i++)
+                    { 
+                        if(fecha1 == feriados[i])
+                        {
+                            Console.WriteLine(fecha1.ToShortDateString() + " no es fin de semana pero es feriado");
+                            esFeriado = true;
+                            break;
+                        }
+                    }
+
+                    if (esFeriado == false)
+                    {
+                        cont++;
+                    }
+
+                    esFeriado = false;
+                }
+            }
+
+            return cont;
+        }
+
+        public DateTime sumarDias(DateTime fecha ,int diasAsumar)
+        {
+            DateTime fechaNueva = fecha.AddDays(diasAsumar);
+
+            return fechaNueva;
+        }
+    }
+}
---
+++ EjercicioFechas/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace EjercicioFechas
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            DateTime fechaUno = new DateTime(2022, 1, 1);
+            DateTime fechaDos = new DateTime(2022, 12, 31);
+
+            GestionFechas gf = new GestionFechas();
+
+            Console.WriteLine("Dias entre fecha1 y fecha2: " + gf.obtenerDiasCalendario(fechaDos, fechaUno));
+
+            Console.WriteLine("Dias laborales: " + gf.obtenerDiasLaborales(fechaUno, fechaDos));
+
+            Console.WriteLine("Sumar 9 dias a 1/01: " + gf.sumarDias(fechaUno, 9));
+
+        } 
+    }
+}
